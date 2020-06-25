ab1145e15faeed808832c3362b09b42fbfd0a507
Administrator <admin@example.com>
2020-06-25 18:04:03 -0500

    - Conversion to Slack - Changed main communication layer to be RTM compatiable. - Started changing text formatting to be a little more 21st century like. - Changed username to utilize userid's and format them as such in the message (slack automagically converts id's to display names). - Changed utils.chat to always send to DM's unless it's a world event, in which it'll post to the #slack-rpg channel.

--- bot.py
+++ bot.py
@@ -21,10 +21,10 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 logging.basicConfig()
 # SlackRPG token
-#slack_client = SlackClient("xoxb-1181626063335-1191204522711-RyTDywZOmBn1nFkDz39uVypz")
+#slack_client = SlackClient("")
 
 #Peak6 Token
-slack_client = SlackClient("xoxb-2471115697-1199711534550-GKkLn987oy5tNwB5UyVJVfh4")
+slack_client = SlackClient("")
 starterbot_id = None
 RTM_READ_DELAY = 1  # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
