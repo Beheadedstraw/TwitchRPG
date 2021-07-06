a75466b65755d09c9eb29a6c0ec1c91355c27c1d
Beheadedstraw <devin.arendt@gmail.com>
2021-07-06 12:39:30 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,9 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 logging.basicConfig()
 # SlackRPG token
-#slack_client = SlackClient("xoxb-1181626063335-1191204522711-RyTDywZOmBn1nFkDz39uVypz")
 
 #Peak6 Token
-slack_client = SlackClient("xoxb-2471115697-1199711534550-CiE97XRiy8hg7Kzp0SWY2kOy")
+slack_client = SlackClient("")
 starterbot_id = None
 RTM_READ_DELAY = 1  # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
